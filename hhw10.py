7b49305397c87b8f69d42977c3da8e483509c37f
qquinyri1 <quinyrii@gmail.com>
2023-06-12 13:18:38 +0300

    Update hhw10.py

--- hhw10.py
+++ hhw10.py
@@ -1,57 +1,47 @@
-import json 
 from collections import UserDict
 
 class AddressBook(UserDict):
-    def __init__(self):
-        super().__init__()
-        self.load_data()
-    
-    def load_data(self):
-        try:
-            with open('address_book.json','r') as fh:
-                self.data = json.load(fh)
-        except FileNotFoundError:
-            self.data = {}
-    
-    def save_data(self):
-        with open('address_book.json', 'w') as fh:
-            json.dump(self.data, fh, indent=4)
-    
-    def add_record(self,record):
-        self.data[record.name.value] = record.data
-        self.save_data()
-
-class Field:
-    def __init__(self, value) -> None:
-        self.value = value
-    
-class Name(Field):
-    def __init__(self, value) -> None:
-        super().__init__(value)
-
-class Phone(Field):
-    def __init__(self, value) -> None:
-        super().__init__(value)
+    def add_record(self, record):
+        self.data[record.name.value] = record
 
 class Record:
-    def __init__(self, name) -> None:
-        self.name = Name(name)
-    
-    def add_phone(self, phone):
-        self.phones.append(Phone(phone))
-    
-    def remove_phone(self,phone):
-        self.phones.append(Phone(phone))
+    def __init__(self, name, *fields):
+        self.name = name
+        self.fields = list(fields)
 
-    def edit_phone(self, old_phone, new_phone):
-        for phone in self.phones:
-            if phone.value == old_phone:
-                phone.value = new_phone
+    def add_field(self, field):
+        self.fields.append(field)
 
-    @property
-    def data(self):
-        return {
-            'name': self.name.value,
-            'phones': [phone.value for phone in self.phones]
-        }
+    def delete_fields(self, field):
+        if field in self.fields:
+            self.fields.remove(field)
 
+    def edit_fields(self, old_field, new_field):
+        if old_field in self.fields:
+            index = self.fields.index(old_field)
+            self.fields[index] = new_field
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+
+if __name__ == "__main__":
+    # main()
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].fields, list)
+    assert isinstance(ab['Bill'].fields[0], Phone)
+    assert ab['Bill'].fields[0].value == '1234567890'
+    print('All Ok)')
